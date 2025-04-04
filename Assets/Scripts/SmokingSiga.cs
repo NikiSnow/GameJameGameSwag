fbaff528881abcae71e8d47059928bdecff0513a
NikiSnow <snow.12356@ya.ru>
2025-04-05 02:56:10 +0300

    web swap

--- Assets/Scripts/SmokingSiga.cs
+++ Assets/Scripts/SmokingSiga.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class SmokingSiga : MonoBehaviour
+public class SmokingSiga : MonoBehaviour, IPointerClickHandler
 {
-    
+    private GameObject Siga;
+    [SerializeField] private GameObject Chapman;
+    bool picked= false;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!picked)
+        {
+            Debug.Log("siga pick me!");
+            Destroy(Chapman);
+        }
+
+    }
+
 }
