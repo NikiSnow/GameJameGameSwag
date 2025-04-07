43b964739004edfe1a8511ea44f8ac2fc29cbd78
NikiSnow <snow.12356@ya.ru>
2025-04-07 22:59:13 +0300

    Swagchik

---
+++ Assets/Scripts/Enemy/TutorialEnemyt.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEnemyt : MonoBehaviour
+{
+    [SerializeField] private EnemyLogic TutorialEnemy;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TutorialEnemy._isPaused = false;
+        }
+    }
+}
